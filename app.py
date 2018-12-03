451e9445909a31518c458561af483cdf499a5e67
Unknown <anthony.marechal@yahoo.fr>
2018-12-03 13:32:50 +0100

    :construction: Update description

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+#
+# Foobar.py: Description of what foobar does.
+#
+
+__author__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__copyright__ = "Copyright 2018, FuzeProg, Liinoa"
+__credits__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__version__ = "1.0.1"
+__maintainer__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__email__ = ["anthony.marechal@etu.uphf.fr", "ombeline.mozdzierz@etu.uphf.fr"]
+__status__ = "In product"
+
