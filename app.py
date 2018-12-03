2f20742da942ed94b156c007a05bd26d1da489a5
Unknown <anthony.marechal@yahoo.fr>
2018-12-03 14:06:16 +0100

    :construction: WIP bellman tests

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # _*_ coding: utf8 _*_
 #
 # Foobar.py: Description of what foobar does.
 #
 
-__author__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
-__copyright__ = "Copyright 2018, FuzeProg, Liinoa"
-__credits__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
+__author__ = "Anthony MARECHAL"
+__copyright__ = "Copyright 2018, FuzeProg"
+__credits__ = ["Anthony MARECHAL"]
 __version__ = "1.0.1"
-__maintainer__ = ["Anthony MARECHAL", "Ombeline MOZDZIERZ"]
-__email__ = ["anthony.marechal@etu.uphf.fr", "ombeline.mozdzierz@etu.uphf.fr"]
+__maintainer__ = "Anthony MARECHAL"
+__email__ = "anthony.marechal@etu.uphf.fr"
 __status__ = "In product"
-
-def func(param):
-    return param
-
